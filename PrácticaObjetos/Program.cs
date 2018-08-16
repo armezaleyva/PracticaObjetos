4af42a4ea6c0ba7c1ba0172f0cb9b6747ecc0476
Armando Meza Leyva <armezaleyva@gmail.com>
2018-08-16 08:36:24 -0700

    Constructores

--- PrácticaObjetos/Program.cs
+++ PrácticaObjetos/Program.cs
@@ -7,27 +7,32 @@ using System.Threading.Tasks;
 namespace PrácticaObjetos {
     class Program {
         static void Main(string[] args) {
-            Persona persona1 = new Persona();
-            persona1.nombre = "Juan";
-            persona1.apellidos = "Escutia";
-            persona1.edad = 20;
+            Contacto contacto1 = new Contacto();
 
-            Console.WriteLine(persona1.nombre + " " + persona1.apellidos
-                              + " tiene " + persona1.edad + " años.");
+            contacto1.Nombre = "Pedro Lopez";
+            contacto1.TelefonoCasa = "(644) 115 - 2514";
+            contacto1.Direccion = "Guerrero 456";
 
-            persona1.cumplirAños();
-            Console.WriteLine("Ahora " + persona1.nombre + " " + persona1.apellidos
-                              + " tiene " + persona1.edad + " años.");
+            Console.WriteLine("Nombre: " + contacto1.Nombre);
+            Console.WriteLine("Teléfono de casa:  " + contacto1.TelefonoCasa);
 
-            persona1.altura = 1.5f;
+            Contacto contacto2 = new Contacto("Juan Perez");
 
-            Console.WriteLine(persona1.nombre + " mide: " + persona1.altura);
+            Console.WriteLine("Contacto 2");
+            Console.WriteLine("Nombre: " + contacto2.Nombre);
 
-            persona1.crecer(0.12f);
+            Contacto contacto3 = new Contacto("Maria Navarro", "1234567890",
+                                              "9876543210", "5 de febrero 895",
+                                              "maria@correo.com");
 
-            Console.WriteLine(persona1.nombre + " ahora mide: " + persona1.altura);
+            Console.WriteLine("Contacto 3");
+            Console.WriteLine("Nombre: " + contacto3.Nombre);
+            Console.WriteLine("Celular:  " + contacto3.TelefonoCelular);
+            Console.WriteLine("Teléfono de casa:  " + contacto3.TelefonoCasa);
+            Console.WriteLine("Direccion: " + contacto3.Direccion);
+            Console.WriteLine("Correp: " + contacto3.CorreoElectronico);
 
-            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
